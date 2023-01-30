72c2d25eda530693ddbc084e9e394b1d1360ec7e
azeddine11 <azeddineaittaakat1r@gmail.com>
2023-01-30 20:32:29 +0100

    done again

--- main.js
+++ main.js
@@ -3,27 +3,47 @@
 WITH ARRAYS
 */
 
-function distinctElement() {
+  var arr1 = [3,1,7,9]; 
+  var arr2 = [2,4,1,9,3];
+  let arr3 = arr1.concat(arr2);
+  let the_one = [];
+  let sum = 0;
 
-    var arr1 = [3,1,7,9,5]; 
-    var arr2 = [2,4,1,9,3,5];
-    let sum = 0;
-    let arr3 = [];
-    let two_arr = arr1.concat(arr2);
-  
-    // push the element that not already exist in arr3 
-    for (let i = 0; i < two_arr.length; i++) {
-      if (!arr3.includes(two_arr[i])) {  
-        arr3.push(two_arr[i]);
+
+//loop inside loop to know the element that not distinct 
+  for(let i=0;i < arr1.length; i++)
+  {
+    for(let j=0;j < arr2.length; j++)
+    {
+      if(arr1[i]== arr2[j])
+      {
+        the_one.push(arr1[i])
       }
+
     }
-    
-    // sum of elements
-    for (let i = 0; i < arr3.length; i++) {
-      sum += arr3[i];
-    }  
-    console.log(sum);
+
   }
+
+
+/* loop inside loop to give 0 to the element that 
+are exist in  the_one array and in arr3 */
+  for(let i=0;i < arr3.length; i++)
+  {
+    for(let j=0;j < the_one.length; j++)
+    {
+      if(arr3[i] === the_one[j])
+      {
+        arr3[i] = 0 
+
+      }
+
+    }
+    sum += arr3[i]
+  }
+
+  console.log(sum)
+
+
 
 
 
@@ -48,12 +68,12 @@ for (let key in obj1) {
     }
 }
 // Calculate the sum of the overlapping elements and store it in a variable
-let sum = 0;
+let summ = 0;
 for (let key in twice) {
-    sum += parseInt(key);
+    summ += parseInt(key);
 }
 
 // Print the sum
-console.log(sum);
+console.log(summ);
 
   
